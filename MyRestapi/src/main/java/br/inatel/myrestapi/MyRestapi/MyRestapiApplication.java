b5e38baa9de5d0f5a8009552bfaa118648e85da5
Cividati <rubens.cividati.teles@gmail.com>
2022-03-12 14:28:46 -0300

    feat: create spring project

---
+++ MyRestapi/src/main/java/br/inatel/myrestapi/MyRestapi/MyRestapiApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.myrestapi.MyRestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestapiApplication.class, args);
+	}
+
+}
---
+++ MyRestapi/src/test/java/br/inatel/myrestapi/MyRestapi/MyRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.myrestapi.MyRestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
