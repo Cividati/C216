163243e58e52b43e6b75fe04fa3e5ea6185aee8f
Cividati <rubens.cividati.teles@gmail.com>
2022-03-19 18:31:14 -0300

    test: add validaCurso method

--- MyRestapi/src/test/java/br/inatel/myrestapi/MyRestapi/controller/CursoControllerTest.java
+++ MyRestapi/src/test/java/br/inatel/myrestapi/MyRestapi/controller/CursoControllerTest.java
@@ -1,9 +1,12 @@
 package br.inatel.myrestapi.MyRestapi.controller;
 
+import br.inatel.myrestapi.model.Curso;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CursoControllerTest {
@@ -20,5 +23,21 @@ public class CursoControllerTest {
                 .isOk()
                 .expectBody()
                 .returnResult();
+    }
+
+    @Test
+    public void validaCurso(){
+        Long idCurso = 1L;
+
+        Curso curso = webTestClient.get()
+                .uri("/curso/" + idCurso)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Curso.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertEquals(curso.getId(), idCurso);
     }
 }
