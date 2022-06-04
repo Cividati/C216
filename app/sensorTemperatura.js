4bed7330ef87c05aac2ab2fdf11f99c1bc7e6c43
Cividati <rubens.cividati.teles@gmail.com>
2022-06-04 15:06:59 -0300

    feat: update sensorTemperatura

--- app/sensorTemperatura.js
+++ app/sensorTemperatura.js
@@ -3,8 +3,5 @@ export class sensorTemperatura {
     getTemperatura(){
         return Math.floor(Math.random() * 100);
     }
-    constructor(altura, largura) {
-      this.altura = altura;
-      this.largura = largura;
-    }
+
   }
