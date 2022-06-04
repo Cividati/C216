9e4bb9fec6d7cdf5f67da04aa82e0bab358a1ba9
Cividati <rubens.cividati.teles@gmail.com>
2022-06-04 15:07:40 -0300

    remove whitespaces

--- app/subscriber.js
+++ app/subscriber.js
@@ -5,7 +5,7 @@ const client  = mqtt.connect('mqtt://localhost:1883')
 client.subscribe('sensor')
 
 client.on('message', function (topic, message) {
-  // message is Buffer
+
   console.log(message.toString())
-  // client.end()
+
 })
