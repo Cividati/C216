cc2e3fc3dc577122e2d1462a1bff267b32dfe6ec
Cividati <rubens.cividati.teles@gmail.com>
2022-06-11 13:57:29 -0300

    blank branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# C216
-Repositório destinado a atividades da disciplina de Sistemas Distribuidos
--- app/sensorTemperatura.js
+++
@@ -1,7 +0,0 @@
-export class sensorTemperatura {
-
-    getTemperatura(){
-        return Math.floor(Math.random() * 100);
-    }
-
-  }
